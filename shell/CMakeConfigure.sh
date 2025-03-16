bb9bd547903113ac723c6a98ca5eedeae5920546
DogGuyMan <escatrgot@gmail.com>
2025-03-16 21:07:22 +0900

    add : vcpkg cmake

--- shell/CMakeConfigure.sh
+++ shell/CMakeConfigure.sh
@@ -1,1 +1,4 @@
 cmake -G "Unix Makefiles" -B build
+# cmake {project_dir} -DCMAKE_TOOLCHAIN_FILE={util_dir}\vcpkg\scripts\buildsystems\vcpkg.cmake
+# CMake projects should use: "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+# cmake -B build -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
---
+++ shell/CMakeMenifest.sh
@@ -0,0 +1,3 @@
+vcpkg add port spdlog
+vcpkg add port opencv4
+vcpkg integrate install
